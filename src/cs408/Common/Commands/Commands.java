ca8c6be3022f908b2005a8d42ada052c22855162
Burak Ayaz <bayaz@sabanciuniv.edu>
2017-11-08 23:36:23 +0300

    Removed unnecessary constructor.

--- src/cs408/Common/Commands/Commands.java
+++ src/cs408/Common/Commands/Commands.java
@@ -3,9 +3,6 @@ package cs408.Common.Commands;
 import java.util.ArrayList;
 
 public class Commands extends ArrayList<Command> {
-    public Commands() {
-    }
-
     public Command find(String name) {
         return this.stream().filter(
                 command -> command.getName().equals(name)
